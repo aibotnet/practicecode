9065db92b5c0106847e877314b07d5ada3d65588
vikas kumar thakur <vikas.dumca2012@gmail.com>
2015-02-18 23:12:35 +0530

    Minor change in SearchBitonicArray.cpp file

--- SearchInBitonicArray.cpp
+++ SearchInBitonicArray.cpp
@@ -3,15 +3,17 @@
 #include<stdbool>
 using namespace std;
 
-bool searchElementInBitonicArrayUtil(int arr[],int start,int end){
-		
+bool searchElementInBitonicArrayUtil(int arr[],int start,int end, int el){
+	int mid= start+(end-start)/2;
+	if(start>end) return false;
+	if(arr[mid]==el) return true;	
 }
 
-bool searchElementInBitonicArray(int arr[],int n){
-	return searchElementInBitonicArrayUtil(arr,0,n-1);
+bool searchElementInBitonicArray(int arr[],int n, int el){
+	return searchElementInBitonicArrayUtil(arr,0,n-1, el);
 }
 
 int main(){
 	int arr = [1,2,3,4,5,6,11,14,24,20,17,6,5];
-	searchElementInBitonicArray(arr,sizeof(arr)/sizeof(int));
+	searchElementInBitonicArray(arr,sizeof(arr)/sizeof(int),2);
 }
