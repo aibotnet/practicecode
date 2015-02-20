88a4a63d20db1d7f517324160172200dfaf825db
vikas kumar thakur <vikas.dumca2012@gmail.com>
2015-02-20 23:59:42 +0530

    New nlogn approach is added for printPathArrayToElement

--- binary-tree/printPathArrayToElement.cpp
+++ binary-tree/printPathArrayToElement.cpp
@@ -9,9 +9,43 @@ struct node{
 };
 typedef struct node node;
 
+//return path in array for BST
+//Time comlexity o(nlogn)
+bool getPathArrayBST(node* root,int arr[],int *len, int se){
+	if(!root) return false;
+	if(root->data==se){ // preorder checking elemnt
+		arr[*len]=2;//this indicate end of traversal
+		*len=*len+1;
+		return true;
+	}
+	else if(root->data > se){
+		bool left = getPathArrayBST(root->left,arr,len,se);
+		if (left){arr[*len]=1;*len=*len+1;return true;}
+		return false;
+	}
+	else{
+		bool right = getPathArrayBST(root->right,arr,len,se);
+		if(right){arr[*len]=0;*len=*len+1;return true;}
+		return false;
+	}
+}
 
-void getPathArray(node* root,char *arr,int *len){
+//path array in binary tree
+//return the reverse of path
+//Time compexity o(n)
+bool getPathArray(node* root,int arr[],int *len, int se){
+	if(!root) return false;
+	bool left = getPathArray(root->left,arr,len,se);
+	bool right = getPathArray(root->right,arr,len,se);
+	if(root->data==se){ // preorder checking elemnt
+		arr[*len]=2;//this indicate end of traversal
+		*len=*len+1;
+		return true;
+	}
+	if(left){arr[*len]=1;*len=*len+1;return true;}
+	if(right){arr[*len]=0;*len=*len+1;return true;}
 
+	return false;
 }
 
 void inorder(node* root){
@@ -42,6 +76,13 @@ int main(){
 	root->right->left->right= getNewNode(37);
 	root->right->right= getNewNode(44);
 	root->right->right->right= getNewNode(46);
-	inorder(root);
-	getPathArray(arr);
+	//inorder(root);
+	int arr[256]={0};int len=0;
+	getPathArrayBST(root,arr,&len,18);
+	//printing array content
+	cout<<"Path Array\n";
+	for(int i=0;i<len;i++){
+		cout<<arr[i]<<" ";
+	}
+	return 0;
 }
