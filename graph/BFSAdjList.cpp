cc1f49fe4ed0348c1590e551b37fe556b11fda5b
vikas kumar thakur <vikas.dumca2012@gmail.com>
2015-03-05 16:24:18 +0530

    BFS adj added

---
+++ graph/BFSAdjList.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include <list>
+#include <queue> 
+using namespace std;
+ 
+// Graph class represents a directed graph using adjacency list representation
+struct graph{
+    int v;
+    list<int> *adj;
+}g;
+ 
+ void initialize(int v){
+    g.v=v;
+    g.adj=new list<int>[v];
+ }
+
+void addEdge(int v, int w){
+    g.adj[v].push_back(w); // Add w to v’s list.
+}
+ 
+
+ 
+void BFS(int s){
+    bool *visited = new bool[g.v];
+    for(int i = 0; i < g.v; i++)
+        visited[i] = false;
+    queue<int> q;
+    visited[s] = true;
+    q.push(s);
+
+    list<int>::iterator i; 
+    while(!q.empty()){
+        // Dequeue a vertex from queue and print it
+        s = q.front();
+        cout << s << " ";
+        q.pop();
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = g.adj[s].begin(); i != g.adj[s].end(); ++i){
+            if(!visited[*i]){
+                visited[*i] = true;
+                q.push(*i);
+            }
+        }
+    }
+}
+ 
+int main(){
+    // Create a graph given in the above diagram
+    initialize(4);
+    addEdge(0, 1);
+    addEdge(0, 2);
+    addEdge(1, 2);
+    addEdge(2, 0);
+    addEdge(2, 3);
+    addEdge(3, 3);
+ 
+    cout << "Following is Depth First Traversal (starting from vertex 2) \n";
+    BFS(2);
+ 
+    return 0;
+}
